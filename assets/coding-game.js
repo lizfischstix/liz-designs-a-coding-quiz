1cc69ba1b60b5f8d8369cfba283e10a78f4eda33
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-09-25 17:03:43 -0500

    changed structure, added js file for questions

--- assets/coding-game.js
+++ assets/coding-game.js
@@ -1,46 +1,76 @@
 //TODO: Timer
+// variables to track state of our quiz
+var currentQuestion = 0;
+var time = questions.length * 20;
+//variables to reference DOM elements
+var timerEl = document.getElementById('time');
+var questionsEl = document.getElementById('questions');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start')
+var initialsEl = document.getElementById("initials");
+var timerId;
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds";
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds";
   
-      if((secondsLeft === 0) ||  {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-    }, 6000);
-  }
-//TODO: picture carusel with answer buttons/-10sec for false selection
-var carousel = document.querySelector(.carouselbox);
+//       if((secondsLeft === 0) ||  {
+//         clearInterval(timerInterval);
+//         sendMessage();
+//       }
+//     }, 6000);
+//   }
+function startQuiz() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class")
+    timerId = setInterval(function(){
+      time--;
+      timerEl.textContent = time;
+    }, 1000)
+
+    if(time <= 0){ //|| all questions answered
+      //quiz to stop
+      clearInterval(timerId)
+      var endScreenEl = document.getElementById("end-screen")
+      endScreenEl.removeAttribute('class');
+      //show score
+      var finalScoreEl = document.getElementById('final-score')
+    }
+}
+
 //FOR hiscore.html
 var player = document.getElementById("player-names");
 var score = document.getElementById("score");
 
-function SaveHiScore() {
-var hiScore = {
-    player: player.value,
-    score: score.value,
-}
-    localStorage.setItem('hiScore', JSON.stringify(hiScore));
-}
-//TODO:attach js to html
-function renderHiScore() {
-    var lastScore = JSON.parse(localStorage.getItem('hiScore'));
-    // Check if data is returned, if not exit out of the function
-    if (lastScore !== null) {
-      document.getElementById('saved-name').innerHTML = lastScore.player;
-      document.getElementById('saved-score').innerHTML = lastScore.score;
-    }
-  }
+// function SaveHiScore() {
+// var hiScore = {
+//     player: player.value,
+//     score: score.value,
+// }
+//     localStorage.setItem('hiScore', JSON.stringify(hiScore));
+// }
+// //TODO:attach js to html
+// function renderHiScore() {
+//     var lastScore = JSON.parse(localStorage.getItem('hiScore'));
+//     // Check if data is returned, if not exit out of the function
+//     if (lastScore !== null) {
+//       document.getElementById('saved-name').innerHTML = lastScore.player;
+//       document.getElementById('saved-score').innerHTML = lastScore.score;
+//     }
+//   }
 
-  saveButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    SaveHiScore();
-    renderHiScore();
-  });
+//   saveButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     SaveHiScore();
+//     renderHiScore();
+//   });
 
-  function init() {
-    renderHiScore();
-  }
-  init();
+//   function init() {
+//     renderHiScore();
+//   }
+//   init();
+
+  startBtn.onclick = startQuiz;
---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: "Inside which HTML element is the  .JS file attached?",
+        choices: ["<script>", "<js>", "<java-script>", "<java>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where in the HTML file should external JavaScript be linked?",
+        choices: ["top of <head> element", "bottom of <head> element", "top of <body> element", "bottom of <body> element"],
+        answer: "bottom of <body> element"
+    },
+    {
+        title: "What function would render a box with the message "Hello World"",
+        choices: ["msg("Hello World")", "alert("Hello World")", "function{"Hello World"}", "alert-box("Hello World")"],
+        answer: "alert("Hello World")"
+    }
+]
